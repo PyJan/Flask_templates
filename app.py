41449a173c5f81fa49174e0642f5cf33b123855b
Jan <jan.svarcbach@gmail.com>
2018-10-10 22:36:04 +0200

    template structures

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def main():
     return render_template('main.html')
 
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+@app.route('/child')
+def child():
+    return render_template('child.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
