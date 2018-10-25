6fb8a6d6610f449e0a276c5c9c4f9b59ee67db6d
Jan <jan.svarcbach@gmail.com>
2018-10-26 01:04:30 +0200

    Boheh plot
    creates bokeh multiline plot

--- models/gbm.py
+++ models/gbm.py
@@ -4,11 +4,20 @@
 Created on Thu Oct 25 19:28:54 2018
 
 @author: jan
-"""
 
-"""
 contains class for simulation of Geometric Brownian Motion
 """
+
+import pandas as pd
+import numpy as np
+
+from pandas import DataFrame
+from bokeh.plotting import show, figure
+from bokeh.io import reset_output
+from bokeh.palettes import Paired12
+
+
+
 
 class GBM():
     """
@@ -17,7 +26,7 @@ class GBM():
     formula: dS / S = mu * dt + sigma * dZ         
     """
     
-    def __init__(self, mu=0.05, sigma=0.01, S0=50, dt=1, steps=50, numScen=10):
+    def __init__(self, mu=0.05, sigma=0.01, S0=50, dt=0.02, steps=50, numScen=10):
         """
         parameters:
             
@@ -36,6 +45,35 @@ class GBM():
         self._numScen = numScen
         self._sim = None
         
+    def calculateScenarios(self):
+        """
+        sceate scenarios and fill self._sim with them
+        """
+        self._sim = np.zeros((self._steps, self._numScen))
+        self._sim[0] = self._S0
+        for i in range(self._steps-1):
+            dZ = np.random.normal(scale=np.sqrt(self._dt), size=self._numScen)
+            self._sim[i+1] = ((self._mu * self._dt + self._sigma * dZ) 
+                               * self._sim[i] + self._sim[i])
+        columns = ['S{:03d}'.format(i+1) for i in range(self._numScen)]
+        self._sim = DataFrame(self._sim, columns=columns, 
+                              index=np.arange(self._steps)*self._dt)
+    
+    def createPlot(self):
+        """
+        create Bokeh plot of scenarios
+        """
+        p = figure(width=400, height=400)
+        p.multi_line(xs=[self._sim.index]*self._numScen,
+                     ys=[self._sim[col].values for col in self._sim],
+                     line_width=2, 
+                     line_color=(Paired12*(self._numScen//12+1))[:self._numScen])
+        return p
         
-if __name__ = '__main__':
+        
+if __name__ == '__main__':
     gbm = GBM()
+    gbm.calculateScenarios()
+    reset_output()
+    p = gbm.createPlot()
+    show(p)
